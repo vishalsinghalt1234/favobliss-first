b83631792b27ad5cfd302d6a2053532a5a47ae75
Vishal Singh <vishalsingh173911@gmail.com>
2026-02-04 13:28:25 +0530

    image optimization edited for custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images : {
-        unoptimized : true,
+         unoptimized: false,  // ✅ Enable optimization
+        formats: ['image/avif', 'image/webp'],
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        minimumCacheTTL: 60,
         remotePatterns : [
             {
                 protocol : "https",
                 hostname : "res.cloudinary.com",
+            },
+            {
+                protocol: "https",
+                hostname: "images.favobliss.com",  // ✅ Your custom domain
             },
              {
                 protocol : "https",
@@ -16,6 +24,7 @@ const nextConfig = {
     experimental: {
         missingSuspenseWithCSRBailout: false,
     },
+    
 };
 
 export default nextConfig;
