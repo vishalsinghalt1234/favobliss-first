67408f402e5f70902c843fe9b0b660495edf650f
Vishal Singh <vishalsingh173911@gmail.com>
2026-02-04 12:23:04 +0530

    image optimization removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images : {
-         unoptimized: false,  // ✅ Enable optimization
-        formats: ['image/avif', 'image/webp'],
-        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-        minimumCacheTTL: 60,
+        unoptimized : true,
         remotePatterns : [
             {
                 protocol : "https",
@@ -19,28 +15,6 @@ const nextConfig = {
     },
     experimental: {
         missingSuspenseWithCSRBailout: false,
-    },
-    async headers() {
-        return [
-            {
-                source: '/assets/:path*',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=31536000, immutable',
-                    },
-                ],
-            },
-            {
-                source: '/_next/image',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=31536000, immutable',
-                    },
-                ],
-            },
-        ];
     },
 };
 
